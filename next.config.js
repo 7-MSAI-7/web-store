aba26a06b1971b164448e3a6020fdcbe3981a74c
HanByeol Jang <j.1star.0726@gmail.com>
2025-06-15 00:24:12 +0900

    feat: app router 구조에 맞춰 설정 수정

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/web-store',
-  assetPrefix: '/web-store/',
+    
 }
 
 module.exports = nextConfig 
